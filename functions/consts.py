8d748db21ae4df67c4f6b945818557d6d39433ac
Bembnias <BieniasMt@gmail.com>
2024-03-12 17:26:41 +0100

    Added distance measurement functionality

---
+++ functions/consts.py
@@ -0,0 +1,23 @@
+# WARTOŚCI PODANE W centrymetrach
+KNOWN_WIDTHS = {
+    'cell phone': 7.0,
+    'person': 55.0,
+    'tv': 110.0,
+}
+
+# Odległość od kamery (cm - taka jak zmierzono na zdjęciu referencyjnym)
+KNOWN_DISTANCES = {
+    'cell phone': 60.0,
+    'person': 60.0,
+    'tv': 60.0,
+}
+
+# Ścieżki do obrazów referencyjnych
+REFERENCE_IMAGES = {
+    'cell phone': './assets/person_ref.png',
+    'person': './assets/person_ref.png',
+    'tv': './assets/person_ref.png',
+}
+
+# Słownik do przechowywania obliczonych ogniskowych dla każdego typu obiektu
+focal_lengths = {}
